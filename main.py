9d2c25b81b45d91c887f92ac140268eea4b62ecd
Nolan Bjarne <derjuniorschiemann@gmail.com>
2021-03-16 12:49:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from ursina import *
+from ursina import prefabs
 
 move_speed = 2
 
 app = Ursina()
 
 window.borderless = False
+window.exit_button.enabled = False
 
 camera.fov = 80
 camera.orthographic = False
@@ -22,14 +24,14 @@ class Player(Entity):
         # Horizontal movement for the player (x axis)
         h_move = held_keys['d'] - held_keys['a']
         if (h_move != 0):
-            player.x += h_move * move_speed * time.dt
+            self.x += h_move * move_speed * time.dt
 
         # "Vertical" movement for the player (z axis)
         v_move = held_keys['w'] - held_keys['s']
         if (v_move != 0):
-            player.z += v_move * move_speed * time.dt
+            self.z += v_move * move_speed * time.dt
 
-        camera.position = Vec3(player.x, player.y, player.z)
+        camera.position = Vec3(self.x, self.y, self.z)
 
 class Voxel(Button):
     def __init__(self):
