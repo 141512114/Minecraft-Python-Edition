58a5064996f8abbf371c2e84306237881d7a9d24
Nolan Bjarne <derjuniorschiemann@gmail.com>
2021-03-17 21:58:32 +0100

    Update main.py
    Added basic collision detection and gravity, improved camera control with mouse, fixed some raycast and hitbox issues

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
-from math import cos, sin
+from opensimplex import OpenSimplex
 from ursina import *
 import keyboard
 
-gravity = 13.65
+gravity = 9.8
+
+voxels = []
+
+simplexNoise = OpenSimplex()
 
 class Player(Entity):
-    def __init__(self):
+    def __init__(self, position = (0, 3, 0)):
         super().__init__(
             parent = scene,
             model = 'cube',
-            position = (0, 1, 0),
+            position = position,
             rotation = (0, 0, 0),
             color = color.orange,
             visible_self = False,
@@ -19,91 +23,107 @@ class Player(Entity):
             origin_y = 0
         )
 
+        self.cursor = Entity(
+            parent = camera.ui,
+            model = 'circle',
+            color = color.white,
+            scale = .008
+        )
+
         # Default variables (Initalization)
-        self.player_height = 1.65
+        self.player_height = 1.25
 
         self.move_speed = 2.75
         self.mouse_siv = 33
 
         self.fall_speed = 0
-        self.fall_acc = 8.76
-        self.falling = False
+        self.fall_acc = .23
 
-        self.jump_height = 11.45
+        self.jump_height = .21
         self.jump_speed = 0
-        self.jump_acc = 44
+        self.jump_acc = .865
+
+        self.grounded = False
         self.jumping = False
+
+        self.forward_col_ray = None
+        self.sides_col_ray = None
 
         # Camera settings
         camera.fov = 80
         camera.orthographic = False
 
         # Position the camera where the players head should be
-        camera.position = Vec3(self.x, self.y + self.player_height, self.z)
-        camera.rotation = Vec3(0, 0, 0)
+        camera.position = Vec3(self.position.x, self.position.y + self.player_height, self.position.z)
+        camera.rotation = self.rotation
 
         mouse.locked = True
-        mouse.visible = True
+        mouse.visible = False
 
     def update(self):
         global gravity
 
         # Rotate camera to where "the player is looking"
-        look_roll = camera.rotation.y + mouse.x * self.mouse_siv
-        look_pitch = clamp(camera.rotation.x - mouse.y * self.mouse_siv, -89, 89)
-        camera.rotation = Vec3(look_pitch, look_roll, 0)
-        self.rotation = Vec3(0, camera.rotation.y, 0)
+        camera.rotation_y += mouse.velocity[0] * self.mouse_siv
+        camera.rotation_x -= mouse.velocity[1] * self.mouse_siv
+        camera.rotation_x = clamp(camera.rotation_x, -90, 90)
+        self.rotation_y = camera.rotation_y
 
         # Get the direction we're trying to walk in.
         v_move = held_keys['w'] - held_keys['s']
         h_move = held_keys['d'] - held_keys['a']
-        self.direction = Vec3(self.forward * v_move + self.right * h_move).normalized()
 
-        self.position += self.direction * self.move_speed * time.dt
+        forward_dir = self.forward * v_move
+        sides_dir = self.right * h_move
 
-        # Check if the player still has a floor beneath him
-        if not self.jumping:
-            col_floor = raycast(self.position, self.down, ignore = (self,), distance = .1)
-            if not col_floor.hit:
-                self.falling = True
-            else:
-                self.jumping = False
-                self.jump_speed = 0
+        col_ray_origin = self.world_position
+        col_ray_dist = .25
 
-                self.falling = False
-                self.fall_speed = 0
+        # Forward ray: check if something blocks the way when walking forward or backward
+        self.forward_col_ray = boxcast(col_ray_origin, forward_dir, distance = col_ray_dist, thickness = (1, 1), ignore = (self,))
+        if (self.forward_col_ray.hit):
+            forward_dir = self.forward * 0
 
-        # Check if the player is falling and apply gravity if he is
-        if (self.falling and not self.jumping):
-            self.fall_speed += self.fall_acc * time.dt
-            self.fall_speed = clamp(self.fall_speed, 0, gravity)
-            self.position = Vec3(self.position.x, self.position.y - self.fall_speed * time.dt, self.position.z)
+        # Sides ray: check if something blocks the way when walking sideways
+        self.sides_col_ray = boxcast(col_ray_origin, sides_dir, distance = col_ray_dist, thickness = (1, 1), ignore = (self,))
+        if (self.sides_col_ray.hit):
+            sides_dir = self.right * 0
 
-        if (not self.falling and keyboard.is_pressed('space')):
-            self.jumping = True
+        direction = Vec3(forward_dir + sides_dir).normalized()
 
-        if (self.jumping and not self.falling):
-            self.jump_speed += self.jump_acc * time.dt
-            self.jump_speed = clamp(self.jump_speed, 0, self.jump_height)
-
-            self.position = Vec3(self.position.x, self.position.y + self.jump_speed * time.dt, self.position.z)
-
-            if (self.jump_speed >= self.jump_height):
-                self.jumping = False
-                self.falling = True
+        self.position += direction * self.move_speed * time.dt
 
         # Position the camera where the players head should be
         camera.position = Vec3(self.position.x, self.position.y + self.player_height, self.position.z)
 
-    def input(self, key):
-        ray_hit = raycast(camera.position, camera.forward, ignore = (self,), distance = 6)
-        if (ray_hit.hit):
-            col_entity = ray_hit.entity
-            if (key == 'left mouse down'):
-                if (col_entity.type == 'Voxel'):
-                    col_entity.remove_durab()
-            elif (key == 'right mouse down'):
-                Voxel(position = col_entity.position + mouse.normal)
+        # Gravity: check if any floor exists and if not, the player falls
+        bottom_cast = boxcast(Vec3(self.position.x, self.position.y - (self.scale_y / 2), self.position.z), self.down, distance = .1, thickness = (1, 1), ignore = (self,))
+        if not bottom_cast.hit:
+            self.grounded = False
+        else:
+            self.fall_speed = 0
+            self.jump_speed = 0
+            self.reduce_jump_vel = 0
+            self.grounded = True
+
+        if not self.grounded and not self.jumping:
+            self.fall_speed += self.fall_acc * time.dt
+            self.fall_speed = clamp(self.fall_speed, 0, gravity)
+            self.position = Vec3(self.position.x, self.position.y - self.fall_speed, self.position.z)
+
+        if (held_keys['space'] and self.grounded):
+            self.jumping = True
+
+        if (self.jumping):
+            self.jump_speed += self.jump_acc * time.dt
+            self.jump_speed = clamp(self.jump_speed, 0, self.jump_height)
+
+            self.position = Vec3(self.position.x, self.position.y + self.jump_speed, self.position.z)
+
+            if (self.jump_speed >= self.jump_height):
+                self.jumping = False
+
+    
 
 class Voxel(Entity):
     def __init__(self, position = (0, 0, 0), durab = 2):
@@ -115,10 +135,17 @@ class Voxel(Entity):
             color = color.white,
             collider = 'box',
             collision = True,
-            origin_y = 1
+            origin_y = 0
         )
 
         self.durab = durab
+
+    def input(self, key):
+        if (self.hovered and distance(self.position, camera.position) <= 13):
+            if (key == 'left mouse down'):
+                self.remove_durab()
+            elif (key == 'right mouse down'):
+                voxels.append(Voxel(position = self.position + mouse.normal))
 
     def remove_durab(self):
         self.durab -= 1
@@ -131,12 +158,18 @@ window.title = "Minecraft: Python Edition"
 window.borderless = False
 window.exit_button.enabled = False
 window.fps_counter.enabled = True
+window.center_on_screen()
 
 player = Player()
 
 for i in range(-12, 12):
     for j in range(-12, 12):
-        Voxel(position = Vec3(j, 0, i))
+        flatNoise = simplexNoise.noise2d(j, i) / 10
+        hillyNoise = simplexNoise.noise2d(j, i) * 5
+
+        worldNoise = flatNoise * hillyNoise
+
+        voxels.append(Voxel(position = Vec3(j, worldNoise, i)))
     
 
 def update():
